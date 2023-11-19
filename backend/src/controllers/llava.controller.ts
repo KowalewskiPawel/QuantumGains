3340f4a54e5f3a25033a16df8ffe738e56fa31c5
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-11-19 18:40:18 +0100

    chore: add mock flag to analyzePhoto for testing

--- backend/src/controllers/llava.controller.ts
+++ backend/src/controllers/llava.controller.ts
@@ -1,10 +1,15 @@
 import type { Request, Response } from "express";
 import { getBodyFatAndExercises, getSuggestedDiet } from "../llava";
+import { MOCK_LLAVA_PHOTO_RESPONSE } from "../MOCKS";
 
 export class LlavaController {
   async analyzePhoto(req: Request, res: Response) {
     try {
-      const { photoUrl } = req.body;
+      const { photoUrl, isMockRequest } = req.body;
+
+      if (isMockRequest) {
+       return res.status(200).send(MOCK_LLAVA_PHOTO_RESPONSE);
+      };
 
       const bodyFatAndTrainings = JSON.parse(await getBodyFatAndExercises(photoUrl) as unknown as string);
       const suggestedDiet = JSON.parse(await getSuggestedDiet(photoUrl) as unknown as string);
